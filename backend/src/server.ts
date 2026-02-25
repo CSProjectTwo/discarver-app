a886c4b5673d73fe24223c03fc6f6f6a27189a38
Sarib1 <168685591+Sarib1@users.noreply.github.com>
2026-02-25 00:37:04 +0000

    Update server.js to server.ts

---
+++ backend/src/server.ts
@@ -0,0 +1,27 @@
+import express, { Application, Request, Response } from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import connectDB from './config/db';
+import authRoutes from './routes/authRoutes';
+
+dotenv.config();
+connectDB();
+
+const app: Application = express();
+
+// Middlewares
+app.use(express.json());
+app.use(cors());
+
+// Routes
+app.use('/api/auth', authRoutes);
+
+// Test Route
+app.get('/', (req: Request, res: Response) => {
+  res.send('API is running with TypeScript!');
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server active on port ${PORT}`);
+});
