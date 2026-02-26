e6f7edf8760260b50e367141499e9d0491b4bcd3
ilyasin001 <ilymuuh@gmail.com>
2026-02-26 00:16:29 +0000

    server set up

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,27 +1,18 @@
-import express, { Application, Request, Response } from 'express';
+import express from "express";
+import type { Application, Request, Response } from 'express';
 import dotenv from 'dotenv';
-import cors from 'cors';
-import connectDB from './config/db';
-import authRoutes from './routes/authRoutes';
 
 dotenv.config();
-connectDB();
 
 const app: Application = express();
+const PORT = process.env.PORT || 5000;
 
-// Middlewares
-app.use(express.json());
-app.use(cors());
-
-// Routes
-app.use('/api/auth', authRoutes);
 
 // Test Route
 app.get('/', (req: Request, res: Response) => {
   res.send('API is running with TypeScript!');
 });
 
-const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server active on port ${PORT}`);
 });
