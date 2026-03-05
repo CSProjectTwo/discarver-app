1bad9949faa3a3b9e99a284fec58091bc6ccfdb5
Sarib1 <168685591+Sarib1@users.noreply.github.com>
2026-03-05 21:20:58 +0000

    Update server.ts
    Connecting all user id stuff

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -3,6 +3,7 @@ import type { Application, Request, Response } from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
 import connectDB from './config/db.ts';
+import userRoutes from "./routes/userRoutes.ts";
 
 import carRoutes from "./routes/carRoutes.ts"
 
@@ -30,4 +31,11 @@ app.get('/', (req: Request, res: Response) => {
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server active on port ${PORT}`);
+
+app.use(express.json());
+app.use(cors());
+
+app.use("/api", carRoutes);
+app.use("/api/users", userRoutes);
 });
+
