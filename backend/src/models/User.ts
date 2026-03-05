9e2b423138def21cd416d1fae9a5046bde5cea8f
Sarib1 <168685591+Sarib1@users.noreply.github.com>
2026-03-05 21:17:08 +0000

    Create User.ts
    Tells MongoDb what a user looks like

---
+++ backend/src/models/User.ts
@@ -0,0 +1,17 @@
+import mongoose, { Schema } from 'mongoose';
+import bcrypt from 'bcryptjs';
+
+const userSchema = new Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+}, { timestamps: true });
+
+// High Standard: This "scrambles" the password automatically before it hits the database
+userSchema.pre('save', async function(next) {
+  if (!this.isModified('password')) return next();
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
+export const User = mongoose.model('User', userSchema);
