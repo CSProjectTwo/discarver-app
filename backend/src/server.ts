4b5fbaee522254763d51ad6eb0611d864348db5a
ilyasin001 <ilymuuh@gmail.com>
2026-03-10 00:00:05 +0000

    database connection established

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,18 +1,31 @@
 import express from "express";
 import type { Application, Request, Response } from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
+import connectDB from './config/db.ts';
+
 
 dotenv.config();
+connectDB();
 
 const app: Application = express();
-const PORT = process.env.PORT || 5000;
 
+// Middlewares
+app.use(express.json());
+app.use(cors());
+
+
+// Routes
+app.get("/api/test", (req: Request, res: Response) => {
+  res.json({ message: "API is working" });
+});
 
 // Test Route
 app.get('/', (req: Request, res: Response) => {
-  res.send('API is running with TypeScript!');
+  res.send('Connection to database is successful');
 });
 
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server active on port ${PORT}`);
 });
