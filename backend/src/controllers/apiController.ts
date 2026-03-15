68ffeed7c6db8cadfb24337f0ce1bf533121c4d2
ilyasin001 <ilymuuh@gmail.com>
2026-03-15 00:53:11 +0000

    api controller 'fetch models' added

--- backend/src/controllers/apiController.ts
+++ backend/src/controllers/apiController.ts
@@ -3,6 +3,7 @@ import type { Request, Response } from "express";
 import {
   getMakes,
   getEngines,
+  getModels
 } from "../services/apiservices.js";
 
 export async function fetchMakes(req: Request, res: Response) {
@@ -25,5 +26,20 @@ export async function fetchEngines(req: Request, res: Response) {
     res.json(engines);
   } catch {
     res.status(500).json({ error: "Failed to fetch engines" });
+  }
+}
+
+export async function fetchModels(req: Request, res: Response) {
+  try {
+    const year = Number(req.params.year);
+    const make = String(req.params.make);
+    if (isNaN(year)) {
+      res.status(400).json({ error: "year must be a number" });
+      return;
+    }
+    const models = await getModels(make, year);
+    res.json(models);
+  } catch {
+    res.status(500).json({ error: "Failed to fetch models" });
   }
 }
