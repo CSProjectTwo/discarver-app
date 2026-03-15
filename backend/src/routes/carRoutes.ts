6f2432ac95004143c8614863d31e7dccb9861c7c
ilyasin001 <ilymuuh@gmail.com>
2026-03-15 01:11:54 +0000

    isolated user login functions

--- backend/src/routes/carRoutes.ts
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { fetchMakes, fetchModels } from "../controllers/carController.ts";
-
-const router = express.Router();
-
-router.get("/makes/:year", fetchMakes);
-router.get("/models/:year/:make", fetchModels);
-
-export default router;
