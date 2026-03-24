f173cdcbe440d8783d34846c87414744600540e8
ilyasin001 <ilymuuh@gmail.com>
2026-03-24 14:15:21 +0000

    Engine Trim and Model schema created

--- backend/src/models/Listing.ts
+++ backend/src/models/Listing.ts
@@ -1,9 +1,46 @@
 import mongoose, { Schema } from "mongoose";
 
-const MakeSchema = new Schema ();
-const ModelSchema = new Schema ();
-const TrimSchema = new Schema ();
-const EngineSchema = new Schema ();
+const MakeSchema = new Schema (
+    { 
+    api_id: { type: Number, required: true }, 
+    name: { type: String, required: true } 
+    },
+  { _id: false }
+);
+
+const ModelSchema = new Schema (
+    { 
+    api_id: { type: Number, required: true }, 
+    name: { type: String, required: true }, 
+    make_id: { type: Number, required: true } 
+    },
+  { _id: false }
+);
+
+const TrimSchema = new Schema (
+    { 
+    api_id: { type: Number, required: true },
+    name: { type: String },
+    year: { type: Number, required: true },
+    description: { type: String } 
+    },
+  { _id: false }
+);
+
+const EngineSchema = new Schema (
+    {
+    api_id:        { type: Number },
+    engine_type:   { type: String },
+    fuel_type:     { type: String },
+    cylinders:     { type: String },
+    size:          { type: String },
+    horsepower_hp: { type: Number },
+    torque_ft_lbs: { type: Number },
+    drive_type:    { type: String },
+    transmission:  { type: String },
+    },
+    { _id: false }
+);
 const BodySchema = new Schema ();
 
 const ListingSchema = new Schema();
